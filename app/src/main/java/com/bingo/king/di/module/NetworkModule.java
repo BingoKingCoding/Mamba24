496de520f0f10e99814544b24801e106c367c340
BingoKingCoding <634051075@qq.com>
2018-04-13 17:06:23 +0800

    修改debug模式下添加拦截打印log

--- app/src/main/java/com/bingo/king/di/module/NetworkModule.java
+++ app/src/main/java/com/bingo/king/di/module/NetworkModule.java
@@ -1,10 +1,12 @@
 package com.bingo.king.di.module;
 
+import com.bingo.king.BuildConfig;
 import com.bingo.king.app.App;
 import com.bingo.king.app.Constants;
 import com.bingo.king.app.utils.FileUtil;
 import com.bingo.king.mvp.model.http.IRepository;
 import com.bingo.king.mvp.model.http.RepositoryImpl;
+import com.orhanobut.logger.Logger;
 
 import java.util.concurrent.TimeUnit;
 
@@ -25,37 +27,35 @@ import retrofit2.converter.gson.GsonConverterFactory;
  * Created by wang on 2017/11/3 10:30.
  */
 @Module
-public class NetworkModule
-{
+public class NetworkModule {
     private App mApp;
 
-    public NetworkModule(App app)
-    {
+    public NetworkModule(App app) {
         this.mApp = app;
     }
 
     @Provides
     @Singleton
-    IRepository provideRepository(@Named("gankIo") Retrofit gankIoRetrofit, @Named("zhiHu") Retrofit zhiHuRetrofit, @Named("douBan") Retrofit doubanRetrofit)
-    {
+    IRepository provideRepository(@Named("gankIo") Retrofit gankIoRetrofit, @Named("zhiHu") Retrofit zhiHuRetrofit, @Named("douBan") Retrofit doubanRetrofit) {
         return new RepositoryImpl(mApp, gankIoRetrofit, zhiHuRetrofit, doubanRetrofit);
     }
 
 
     @Singleton
     @Provides
-    OkHttpClient.Builder provideOkHttpBuilder()
-    {
+    OkHttpClient.Builder provideOkHttpBuilder() {
         return new OkHttpClient.Builder();
     }
 
 
     @Singleton
     @Provides
-    OkHttpClient provideClient(OkHttpClient.Builder builder)
-    {
-        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
-        loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+    OkHttpClient provideClient(OkHttpClient.Builder builder) {
+        if (BuildConfig.DEBUG) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+            builder.addInterceptor(loggingInterceptor);
+        }
 
         builder.cache(new Cache(FileUtil.getHttpCacheDir(), Constants.HTTP_CACHE_SIZE));
         //设置超时
@@ -64,7 +64,6 @@ public class NetworkModule
         builder.writeTimeout(Constants.HTTP_READ_TIMEOUT, TimeUnit.SECONDS);
         //错误重连
         builder.retryOnConnectionFailure(true);
-        builder.addInterceptor(loggingInterceptor);
         return builder.build();
     }
 
@@ -72,8 +71,7 @@ public class NetworkModule
     @Provides
     @Named("gankIo")
     @Singleton
-    Retrofit provideGankIoRetrofit(OkHttpClient client)
-    {
+    Retrofit provideGankIoRetrofit(OkHttpClient client) {
         String baseUrl = Constants.BASE_API_URL_GANKIO;
 
         Retrofit retrofit = new Retrofit.Builder()
@@ -88,8 +86,7 @@ public class NetworkModule
     @Provides
     @Named("zhiHu")
     @Singleton
-    Retrofit provideZhiHuRetrofit(OkHttpClient client)
-    {
+    Retrofit provideZhiHuRetrofit(OkHttpClient client) {
         String baseUrl = Constants.BASE_API_URL_ZHIHU;
 
         Retrofit retrofit = new Retrofit.Builder()
@@ -104,8 +101,7 @@ public class NetworkModule
     @Provides
     @Named("douBan")
     @Singleton
-    Retrofit provideDouBanRetrofit(OkHttpClient client)
-    {
+    Retrofit provideDouBanRetrofit(OkHttpClient client) {
         String baseUrl = Constants.BASE_API_URL_DOUBAN;
 
         Retrofit retrofit = new Retrofit.Builder()
