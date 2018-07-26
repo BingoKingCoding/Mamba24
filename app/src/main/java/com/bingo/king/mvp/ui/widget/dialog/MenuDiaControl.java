1fe7033f394dbfae6160f639f2b0e6828b61b5f3
leaderbean <634051075@qq.com>
2018-07-26 19:36:23 +0800

    提交代码

---
+++ app/src/main/java/com/bingo/king/mvp/ui/widget/dialog/MenuDiaControl.java
@@ -0,0 +1,31 @@
+package com.bingo.king.mvp.ui.widget.dialog;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <请描述这个类是干什么的>
+ * Created by WangWB on 2018/7/11:23:05.
+ * Email:634051075@qq.com
+ */
+
+public class MenuDiaControl {
+
+    private Context mContext;
+    private String title = "分享到";
+    //share
+    private String mShareTitle = "";
+    private String mShareContent = "";
+    private String mShareImageUrl = "";
+    private String mShareUrl = "";
+    //action
+    private boolean mHineActionAll = false;//是否隐藏action操作栏
+    private List<Boolean> mBooleanList = new ArrayList<>();//单个action的按钮图标和msg
+//    private MoreMenuClickListener mListener;//action回调
+
+
+
+
+}
